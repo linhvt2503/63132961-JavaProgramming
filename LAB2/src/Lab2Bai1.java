80853c5a6cb712309388ec33a46c36e6cae32a0c
linhvt2503 <vtlinh02052003@gmail.com>
2024-02-17 22:03:12 +0700

    Update Lab2Bai1.java

--- LAB2/src/Lab2Bai1.java
+++ LAB2/src/Lab2Bai1.java
@@ -2,6 +2,7 @@
  * 
  */
 
+import java.util.Scanner;
 /**
  * 
  */
@@ -10,9 +11,25 @@ public class Lab2Bai1 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("Nhập hệ số a: ");
+		double a = scanner.nextDouble();
+		System.out.printf("Nhập hệ số b: ");
+		double b = scanner.nextDouble();
+		if(a == 0) {
+			if(b == 0) {
+				System.out.printf("Vô số nghiệm");
+			}
+			else {
+				System.out.printf("Vô nghiệm");
+			}
+		}
+		else {
+			double x = -b/a;
+			System.out.printf("Nghiệm của phương trình: %.2f", x);
+		}
 	}
 
 }
